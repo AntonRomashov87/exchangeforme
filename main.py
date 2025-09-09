e09a37f3ecc87e20f45c3bf3d9735dc826ede0c3
AntonRomashov87 <romashov1987@gmail.com>
2025-09-09 23:47:01 +0300

    main.py

--- main.py
+++ main.py
@@ -130,6 +130,7 @@ def fuel(message):
 def webhook():
     if request.headers.get('content-type') == 'application/json':
         json_string = request.get_data().decode('utf-8')
+        print(f"Received webhook payload: {json_string}")
         update = telebot.types.Update.de_json(json_string)
         bot.process_new_updates([update])
         return '', 200
